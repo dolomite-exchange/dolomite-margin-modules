b3e7b23f1f1860a19068f8cc9f232d876058951a
Corey <corey@dolomite.io>
2023-07-07 16:17:14 -0400

    fixed gitignore

--- dist/src/utils/constructors/dolomite.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getRegistryProxyConstructorParams = void 0;
-function getRegistryProxyConstructorParams(implementationAddress, implementationCalldata, core) {
-    return [implementationAddress, core.dolomiteMargin.address, implementationCalldata];
-}
-exports.getRegistryProxyConstructorParams = getRegistryProxyConstructorParams;
