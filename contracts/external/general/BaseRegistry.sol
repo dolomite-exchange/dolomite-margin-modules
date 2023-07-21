293d4d2324e10e09186ffd3d0c5546393cf4d65c
BowTiedOriole <bowtiedoriole@protonmail.com>
2023-07-21 11:18:07 -0400

    chore: format base registry

--- contracts/external/general/BaseRegistry.sol
+++ contracts/external/general/BaseRegistry.sol
@@ -20,13 +20,13 @@
 
 pragma solidity ^0.8.9;
 
-import {Initializable} from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
-import {Require} from "../../protocol/lib/Require.sol";
-import {OnlyDolomiteMarginForUpgradeable} from "../helpers/OnlyDolomiteMarginForUpgradeable.sol";
-import {ProxyContractHelpers} from "../helpers/ProxyContractHelpers.sol";
-import {IBaseRegistry} from "../interfaces/IBaseRegistry.sol";
-import {IDolomiteRegistry} from "../interfaces/IDolomiteRegistry.sol";
-import {ValidationLib} from "../lib/ValidationLib.sol";
+import { Initializable } from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+import { Require } from "../../protocol/lib/Require.sol";
+import { OnlyDolomiteMarginForUpgradeable } from "../helpers/OnlyDolomiteMarginForUpgradeable.sol";
+import { ProxyContractHelpers } from "../helpers/ProxyContractHelpers.sol";
+import { IBaseRegistry } from "../interfaces/IBaseRegistry.sol";
+import { IDolomiteRegistry } from "../interfaces/IDolomiteRegistry.sol";
+import { ValidationLib } from "../lib/ValidationLib.sol";
 
 /**
  * @title   BaseRegistry
@@ -43,14 +43,14 @@ contract BaseRegistry is
     // ===================== Constants =====================
 
     bytes32 private constant _FILE = "BaseRegistry";
-    bytes32 private constant _DOLOMITE_REGISTRY_SLOT =
-        bytes32(uint256(keccak256("eip1967.proxy.dolomiteRegistry")) - 1); // solhint-disable-line max-line-length
+    bytes32 private constant _DOLOMITE_REGISTRY_SLOT = bytes32(uint256(keccak256("eip1967.proxy.dolomiteRegistry")) - 1); // solhint-disable-line max-line-length
 
     // ===================== Functions =====================
 
     function ownerSetDolomiteRegistry(
         address _dolomiteRegistry
-    ) external onlyDolomiteMarginOwner(msg.sender) {
+    ) external
+    onlyDolomiteMarginOwner(msg.sender) {
         _ownerSetDolomiteRegistry(_dolomiteRegistry);
     }
 
@@ -60,7 +60,9 @@ contract BaseRegistry is
 
     // ===================== Internal Functions =====================
 
-    function _ownerSetDolomiteRegistry(address _dolomiteRegistry) internal {
+    function _ownerSetDolomiteRegistry(
+        address _dolomiteRegistry
+    ) internal {
         Require.that(
             _dolomiteRegistry != address(0),
             _FILE,
