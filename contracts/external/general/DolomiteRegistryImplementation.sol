a2ddb3afa444586bf839264f6f34216b370fa32b
BowTiedOriole <bowtiedoriole@protonmail.com>
2023-07-21 11:27:22 -0400

    chore: dolomite registry implementation format

--- contracts/external/general/DolomiteRegistryImplementation.sol
+++ contracts/external/general/DolomiteRegistryImplementation.sol
@@ -20,14 +20,14 @@
 
 pragma solidity ^0.8.9;
 
-import {Initializable} from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
-import {Require} from "../../protocol/lib/Require.sol";
-import {OnlyDolomiteMarginForUpgradeable} from "../helpers/OnlyDolomiteMarginForUpgradeable.sol";
-import {ProxyContractHelpers} from "../helpers/ProxyContractHelpers.sol";
-import {IDolomiteRegistry} from "../interfaces/IDolomiteRegistry.sol";
-import {IExpiry} from "../interfaces/IExpiry.sol";
-import {IGenericTraderProxyV1} from "../interfaces/IGenericTraderProxyV1.sol";
-import {ValidationLib} from "../lib/ValidationLib.sol";
+import { Initializable } from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+import { Require } from "../../protocol/lib/Require.sol";
+import { OnlyDolomiteMarginForUpgradeable } from "../helpers/OnlyDolomiteMarginForUpgradeable.sol";
+import { ProxyContractHelpers } from "../helpers/ProxyContractHelpers.sol";
+import { IDolomiteRegistry } from "../interfaces/IDolomiteRegistry.sol";
+import { IExpiry } from "../interfaces/IExpiry.sol";
+import { IGenericTraderProxyV1 } from "../interfaces/IGenericTraderProxyV1.sol";
+import { ValidationLib } from "../lib/ValidationLib.sol";
 
 /**
  * @title   DolomiteRegistryImplementation
@@ -41,13 +41,12 @@ contract DolomiteRegistryImplementation is
     OnlyDolomiteMarginForUpgradeable,
     Initializable
 {
+
     // ===================== Constants =====================
 
     bytes32 private constant _FILE = "DolomiteRegistryImplementation";
-    bytes32 private constant _GENERIC_TRADER_PROXY_SLOT =
-        bytes32(uint256(keccak256("eip1967.proxy.genericTraderProxy")) - 1); // solhint-disable-line max-line-length
-    bytes32 private constant _EXPIRY_SLOT =
-        bytes32(uint256(keccak256("eip1967.proxy.expiry")) - 1); // solhint-disable-line max-line-length
+    bytes32 private constant _GENERIC_TRADER_PROXY_SLOT = bytes32(uint256(keccak256("eip1967.proxy.genericTraderProxy")) - 1); // solhint-disable-line max-line-length
+    bytes32 private constant _EXPIRY_SLOT = bytes32(uint256(keccak256("eip1967.proxy.expiry")) - 1); // solhint-disable-line max-line-length
 
     // ==================== Constructor ====================
 
@@ -63,21 +62,21 @@ contract DolomiteRegistryImplementation is
 
     function ownerSetGenericTraderProxy(
         address _genericTraderProxy
-    ) external onlyDolomiteMarginOwner(msg.sender) {
+    )
+    external
+    onlyDolomiteMarginOwner(msg.sender) {
         _ownerSetGenericTraderProxy(_genericTraderProxy);
     }
 
     function ownerSetExpiry(
         address _expiry
-    ) external onlyDolomiteMarginOwner(msg.sender) {
+    )
+    external
+    onlyDolomiteMarginOwner(msg.sender) {
         _ownerSetExpiry(_expiry);
     }
 
-    function genericTraderProxy()
-        external
-        view
-        returns (IGenericTraderProxyV1)
-    {
+    function genericTraderProxy() external view returns (IGenericTraderProxyV1) {
         return IGenericTraderProxyV1(_getAddress(_GENERIC_TRADER_PROXY_SLOT));
     }
 
@@ -87,7 +86,9 @@ contract DolomiteRegistryImplementation is
 
     // ===================== Internal Functions =====================
 
-    function _ownerSetGenericTraderProxy(address _genericTraderProxy) internal {
+    function _ownerSetGenericTraderProxy(
+        address _genericTraderProxy
+    ) internal {
         Require.that(
             _genericTraderProxy != address(0),
             _FILE,
@@ -95,9 +96,7 @@ contract DolomiteRegistryImplementation is
         );
         bytes memory returnData = ValidationLib.callAndCheckSuccess(
             _genericTraderProxy,
-            IGenericTraderProxyV1(_genericTraderProxy)
-                .MARGIN_POSITION_REGISTRY
-                .selector,
+            IGenericTraderProxyV1(_genericTraderProxy).MARGIN_POSITION_REGISTRY.selector,
             bytes("")
         );
         abi.decode(returnData, (address));
