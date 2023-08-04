cb159572a57cb036a766e74b87cc6d3f3308e767
BowTiedOriole <bowtiedoriole@protonmail.com>
2023-08-04 10:44:15 -0400

    feat: finished first draft jUSDC chainlink tests

--- test/external/jones/usdc/JonesUSDCPriceOracleChainlink.ts
+++ test/external/jones/usdc/JonesUSDCPriceOracleChainlink.ts
@@ -1,11 +1,10 @@
 import { BigNumber, BigNumberish } from 'ethers';
-import { CoreProtocol, getDefaultCoreProtocolConfig, setupCoreProtocol } from '../../../utils/setup';
+import { CoreProtocol, getDefaultCoreProtocolConfig, setupCoreProtocol, setupTestMarket } from '../../../utils/setup';
 import {
   CustomTestToken,
   IERC4626, IJonesWhitelistController, IJonesWhitelistController__factory,
-  JonesUSDCIsolationModeUnwrapperTraderV2,
   JonesUSDCIsolationModeVaultFactory, JonesUSDCIsolationModeVaultFactory__factory,
-  JonesUSDCPriceOracleChainlink,
+  JonesUSDCPriceOracleChainlink, JonesUSDCPriceOracleChainlink__factory,
   JonesUSDCRegistry, JonesUSDCRegistry__factory
 } from '../../../../src/types';
 import deployments from '../../../../scripts/deployments.json';
@@ -18,9 +17,8 @@ import {
   snapshot
 } from '../../../utils';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
-import { createTestToken } from '../../../../src/utils/dolomite-utils';
+import { createContractWithAbi, createTestToken } from '../../../../src/utils/dolomite-utils';
 import { createJonesUSDCPriceOracleChainlink } from '../../../utils/ecosystem-token-utils/jones';
-import * as net from 'net';
 import { ethers } from 'hardhat';
 import { expect } from 'chai';
 import { increase } from '@nomicfoundation/hardhat-network-helpers/dist/src/helpers/time';
@@ -28,7 +26,6 @@ import { expectThrow } from '../../../utils/assertions';
 import { ADDRESSES } from '@dolomite-margin/dist/src';
 
 const USDC_PRICE = BigNumber.from('999986050000000000000000000000'); // $0.99998605
-const JONES_USDC_PRICE = BigNumber.from('1040340673281844411'); // $1.02187...
 const CHAINLINK_REGISTRY_ADDRESS = '0x75c0530885F385721fddA23C539AF3701d6183D4';
 const USDC_SCALE_DIFF = BigNumber.from('10').pow(12);
 describe('JonesUSDCPriceOracleChainlink', () => {
@@ -39,6 +36,7 @@ describe('JonesUSDCPriceOracleChainlink', () => {
   let jonesController: IJonesWhitelistController;
   let factory: JonesUSDCIsolationModeVaultFactory;
   let jonesUSDCPriceOracleChainlink: JonesUSDCPriceOracleChainlink;
+  let jonesUSDCPriceOracleChainlinkWithNoTotalSupply: JonesUSDCPriceOracleChainlink;
   let jUSDC: IERC4626;
   let jUSDCWithNoTotalSupply: CustomTestToken;
   let chainlinkRegistry: SignerWithAddress;
@@ -68,7 +66,6 @@ describe('JonesUSDCPriceOracleChainlink', () => {
     retentionFeeBase = await jonesController.BASIS_POINTS();
 
     jUSDC = core.jonesEcosystem!.jUSDC;
-    jUSDCWithNoTotalSupply = await createTestToken();
     chainlinkRegistry = await impersonate(CHAINLINK_REGISTRY_ADDRESS, true);
 
     await core.testEcosystem!.testPriceOracle!.setPrice(core.tokens.usdc!.address, USDC_PRICE);
@@ -136,12 +133,18 @@ describe('JonesUSDCPriceOracleChainlink', () => {
       const totalAssets = await jUSDC.totalAssets();
       const totalSupply = await jUSDC.totalSupply();
 
-      let price = getjUSDCPrice(USDC_PRICE, totalAssets, totalSupply);
+      const price = getjUSDCPrice(USDC_PRICE, totalAssets, totalSupply);
       expect((await jonesUSDCPriceOracleChainlink.getPrice(factory.address)).value).to.eq(price);
     });
 
     it('returns the correct value when jUSDC total supply is 0', async () => {
+      const testToken = await createTestToken();
+      await jonesUSDCRegistry.connect(core.governance).ownerSetJUSDC(testToken.address);
+      await jonesUSDCPriceOracleChainlink.connect(chainlinkRegistry).performUpkeep('0x');
 
+      const usdcPrice = USDC_PRICE.div(USDC_SCALE_DIFF);
+      expect((await jonesUSDCPriceOracleChainlink.getPrice(factory.address)).value)
+        .to.eq(usdcPrice.sub(usdcPrice.mul(retentionFee).div(retentionFeeBase)));
     });
 
     it('fails when token sent is not djUSDC', async () => {
@@ -180,7 +183,7 @@ describe('JonesUSDCPriceOracleChainlink', () => {
     });
   });
 
-  describe.only('#checkUpkeep', async () => {
+  describe('#checkUpkeep', async () => {
     it('works normally', async () => {
       expect((await jonesUSDCPriceOracleChainlink.checkUpkeep('0x')).upkeepNeeded).to.eq(false);
     });
@@ -200,6 +203,23 @@ describe('JonesUSDCPriceOracleChainlink', () => {
     it('returns true when deviation is greater than .25% and lastTimestamp is less than heartbeat', async () => {
       const totalAssets = await jUSDC.totalAssets();
       const totalSupply = await jUSDC.totalSupply();
+
+      const currentExchangeRate = (await jonesUSDCPriceOracleChainlink.getPrice(factory.address)).value;
+      const [lower, upper] = calculateDeviationRange(
+        currentExchangeRate,
+        totalAssets,
+        totalSupply,
+        retentionFee,
+        retentionFeeBase
+      );
+
+      await core.testEcosystem!.testPriceOracle!.setPrice(core.tokens.usdc!.address, upper.add(BigNumber.from('100000000000000000'))); // .1 eth
+      expect((await jonesUSDCPriceOracleChainlink.connect(chainlinkRegistry).checkUpkeep('0x')).upkeepNeeded)
+        .to.eq(true);
+
+      await core.testEcosystem!.testPriceOracle!.setPrice(core.tokens.usdc!.address, lower.sub(BigNumber.from('100000000000000000'))); // .1 eth
+      expect((await jonesUSDCPriceOracleChainlink.connect(chainlinkRegistry).checkUpkeep('0x')).upkeepNeeded)
+        .to.eq(true);
     });
 
     it('returns true when deviation is less than 0.25% and lastTimestamp is more than heartbeat', async () => {
@@ -215,23 +235,123 @@ describe('JonesUSDCPriceOracleChainlink', () => {
 
   describe('#performUpkeep', async () => {
     it('works if greater than heartbeat period', async () => {
+      await increase(11 * 3600);
+      await expectThrow(
+        jonesUSDCPriceOracleChainlink.connect(chainlinkRegistry).performUpkeep('0x'),
+        'JonesUSDCPriceOracleChainlink: checkUpkeep conditions not met'
+      );
+
+      await increase(3600);
+      const NEW_USDC_PRICE = BigNumber.from('100000000000000000000000000000'); // $0.99998605
+      await core.testEcosystem!.testPriceOracle!.setPrice(core.tokens.usdc!.address, NEW_USDC_PRICE);
+
+      await jonesUSDCPriceOracleChainlink.connect(chainlinkRegistry).performUpkeep('0x');
+      const upkeepTimestamp = await getBlockTimestamp(await ethers.provider.getBlockNumber());
+      expect(await jonesUSDCPriceOracleChainlink.latestTimestamp()).to.eq(upkeepTimestamp);
+
+      const totalAssets = await jUSDC.totalAssets();
+      const totalSupply = await jUSDC.totalSupply();
+      const price = getjUSDCPrice(NEW_USDC_PRICE, totalAssets, totalSupply);
+      expect((await jonesUSDCPriceOracleChainlink.getPrice(factory.address)).value)
+        .to.eq(price);
     });
 
     it('works if greater than deviation upperEdge', async () => {
+      const totalAssets = await jUSDC.totalAssets();
+      const totalSupply = await jUSDC.totalSupply();
+
+      const currentExchangeRate = (await jonesUSDCPriceOracleChainlink.getPrice(factory.address)).value;
+      const [lower, upper] = calculateDeviationRange(
+        currentExchangeRate,
+        totalAssets,
+        totalSupply,
+        retentionFee,
+        retentionFeeBase
+      );
+
+      await core.testEcosystem!.testPriceOracle!.setPrice(core.tokens.usdc!.address, upper);
+      await expectThrow(
+        jonesUSDCPriceOracleChainlink.connect(chainlinkRegistry).performUpkeep('0x'),
+        'JonesUSDCPriceOracleChainlink: checkUpkeep conditions not met',
+      );
+
+      await core.testEcosystem!.testPriceOracle!.setPrice(core.tokens.usdc!.address, upper.add(BigNumber.from('100000000000000000'))); // .1 eth
+      await jonesUSDCPriceOracleChainlink.connect(chainlinkRegistry).performUpkeep('0x');
+      const upkeepTimestamp = await getBlockTimestamp(await ethers.provider.getBlockNumber());
+      expect(await jonesUSDCPriceOracleChainlink.latestTimestamp()).to.eq(upkeepTimestamp);
+
+      const price = getjUSDCPrice(upper.add(BigNumber.from('100000000000000000')), totalAssets, totalSupply);
+      expect((await jonesUSDCPriceOracleChainlink.getPrice(factory.address)).value)
+        .to.eq(price);
     });
 
     it('works if less than deviation lowerEdge', async () => {
+      const totalAssets = await jUSDC.totalAssets();
+      const totalSupply = await jUSDC.totalSupply();
+
+      const currentExchangeRate = (await jonesUSDCPriceOracleChainlink.getPrice(factory.address)).value;
+      const [lower, upper] = calculateDeviationRange(
+        currentExchangeRate,
+        totalAssets,
+        totalSupply,
+        retentionFee,
+        retentionFeeBase
+      );
+
+      await core.testEcosystem!.testPriceOracle!.setPrice(core.tokens.usdc!.address, lower.add(BigNumber.from('100000000000000000')));
+      await expectThrow(
+        jonesUSDCPriceOracleChainlink.connect(chainlinkRegistry).performUpkeep('0x'),
+        'JonesUSDCPriceOracleChainlink: checkUpkeep conditions not met',
+      );
+
+      await core.testEcosystem!.testPriceOracle!.setPrice(core.tokens.usdc!.address, lower.sub(BigNumber.from('100000000000000000'))); // .1 eth
+      await jonesUSDCPriceOracleChainlink.connect(chainlinkRegistry).performUpkeep('0x');
+      const upkeepTimestamp = await getBlockTimestamp(await ethers.provider.getBlockNumber());
+      expect(await jonesUSDCPriceOracleChainlink.latestTimestamp()).to.eq(upkeepTimestamp);
+
+      const price = getjUSDCPrice(lower.sub(BigNumber.from('100000000000000000')), totalAssets, totalSupply);
+      expect((await jonesUSDCPriceOracleChainlink.getPrice(factory.address)).value)
+        .to.eq(price);
     });
 
     it('fails when not called by Chainlink', async () => {
+      await expectThrow(
+        jonesUSDCPriceOracleChainlink.connect(core.hhUser1).performUpkeep('0x'),
+        'JonesUSDCPriceOracleChainlink: caller is not Chainlink'
+      );
     });
 
     it('fails when before heartbeat and within deviation range', async () => {
+      await expectThrow(
+        jonesUSDCPriceOracleChainlink.connect(chainlinkRegistry).performUpkeep('0x'),
+        'JonesUSDCPriceOracleChainlink: checkUpkeep conditions not met'
+      );
     });
   });
 
-  function getjUSDCPrice(usdcPrice, assets, totalSupply) {
-    let price = usdcPrice.div(USDC_SCALE_DIFF).mul(assets).div(totalSupply);
+  function getjUSDCPrice(usdcPrice: BigNumber, assets: BigNumber, totalSupply: BigNumber): BigNumber {
+    const price = usdcPrice.div(USDC_SCALE_DIFF).mul(assets).div(totalSupply);
     return price.sub(price.mul(retentionFee).div(retentionFeeBase));
+  }
+
+  function calculateDeviationRange(
+    exchangeRate: BigNumber,
+    assets: BigNumber,
+    totalSupply: BigNumber,
+    retentionFee: BigNumber,
+    retentionFeeBase: BigNumber
+  ): BigNumber[] {
+    const lowerExchangeRate = exchangeRate.mul(9975).div(10000);
+    const upperExchangeRate = exchangeRate.mul(10025).div(10000);
+
+    let lowerEdge = lowerExchangeRate.mul(retentionFeeBase).div(retentionFeeBase.sub(retentionFee));
+    lowerEdge = lowerEdge.mul(totalSupply).div(assets);
+    lowerEdge = lowerEdge.mul(USDC_SCALE_DIFF);
+
+    let upperEdge = upperExchangeRate.mul(retentionFeeBase).div(retentionFeeBase.sub(retentionFee));
+    upperEdge = upperEdge.mul(totalSupply).div(assets);
+    upperEdge = upperEdge.mul(USDC_SCALE_DIFF);
+
+    return [lowerEdge, upperEdge];
   }
 });
