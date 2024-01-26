550937b818800b7f12571f90ae1e19e532a855aa
Corey <corey@dolomite.io>
2024-01-25 20:36:11 -0500

    updated (un)wrapper for PT-wstETH 2024/2025

--- scripts/deploy/safe-transactions/arbitrum/safe-transaction-202.ts
+++ scripts/deploy/safe-transactions/arbitrum/safe-transaction-202.ts
@@ -1,38 +1,161 @@
+import {
+  getPendlePtIsolationModeUnwrapperTraderV2ConstructorParams,
+  getPendlePtIsolationModeWrapperTraderV2ConstructorParams,
+} from '../../../../src/utils/constructors/pendle';
 import { getAndCheckSpecificNetwork } from '../../../../src/utils/dolomite-utils';
 import { Network } from '../../../../src/utils/no-deps-constants';
-import { setupCoreProtocol } from '../../../../test/utils/setup';
+import { CoreProtocol, setupCoreProtocol } from '../../../../test/utils/setup';
 import {
   createFolder,
   DenJsonUpload,
-  deployContractAndSave,
+  deployContractAndSave, EncodedTransaction,
   prettyPrintEncodedDataWithTypeSafety,
   writeFile,
 } from '../../../deploy-utils';
 
+const old2024Unwrapper = '0x05C5A361431A5b141d9f09D761EA4323B651dfB8';
+const old2024Wrapper = '0x30173DbD3A5af41bE2b8462d1D27E18e536D6705';
+const old2025Unwrapper = '0x98F1DA067A80b6CEFE1eeeaFF63972d5140550Df';
+const old2025Wrapper = '0xb88653715b5E53874bC0A569f48eF2D0c01e4a0E';
+
+async function deployPtWstEthJun2024Updates(core: CoreProtocol): Promise<EncodedTransaction[]> {
+  const unwrapperV4 = await deployContractAndSave(
+    core.config.networkNumber,
+    'PendlePtIsolationModeUnwrapperTraderV2',
+    getPendlePtIsolationModeUnwrapperTraderV2ConstructorParams(
+      core,
+      core.pendleEcosystem!.wstEthJun2024.pendleRegistry,
+      core.tokens.wstEth!,
+      core.pendleEcosystem!.wstEthJun2024.dPtWstEthJun2024,
+    ),
+    'PendlePtWstEthJun2024IsolationModeUnwrapperTraderV4',
+  );
+  const wrapperV4 = await deployContractAndSave(
+    core.config.networkNumber,
+    'PendlePtIsolationModeWrapperTraderV2',
+    getPendlePtIsolationModeWrapperTraderV2ConstructorParams(
+      core,
+      core.pendleEcosystem!.wstEthJun2024.pendleRegistry,
+      core.tokens.wstEth!,
+      core.pendleEcosystem!.wstEthJun2024.dPtWstEthJun2024,
+    ),
+    'PendlePtWstEthJun2024IsolationModeWrapperTraderV4',
+  );
+  const transactions = [];
+  transactions.push(
+    await prettyPrintEncodedDataWithTypeSafety(
+      core,
+      core.pendleEcosystem!.wstEthJun2024,
+      'dPtWstEthJun2024',
+      'ownerSetIsTokenConverterTrusted',
+      [old2024Unwrapper, false],
+    ),
+  );
+  transactions.push(
+    await prettyPrintEncodedDataWithTypeSafety(
+      core,
+      core.pendleEcosystem!.wstEthJun2024,
+      'dPtWstEthJun2024',
+      'ownerSetIsTokenConverterTrusted',
+      [old2024Wrapper, false],
+    ),
+  );
+  transactions.push(
+    await prettyPrintEncodedDataWithTypeSafety(
+      core,
+      core.pendleEcosystem!.wstEthJun2024,
+      'dPtWstEthJun2024',
+      'ownerSetIsTokenConverterTrusted',
+      [unwrapperV4, true],
+    ),
+  );
+  transactions.push(
+    await prettyPrintEncodedDataWithTypeSafety(
+      core,
+      core.pendleEcosystem!.wstEthJun2024,
+      'dPtWstEthJun2024',
+      'ownerSetIsTokenConverterTrusted',
+      [wrapperV4, true],
+    ),
+  );
+  return transactions;
+}
+
+async function deployPtWstEthJun2025Updates(core: CoreProtocol): Promise<EncodedTransaction[]> {
+  const unwrapperV4 = await deployContractAndSave(
+    core.config.networkNumber,
+    'PendlePtIsolationModeUnwrapperTraderV2',
+    getPendlePtIsolationModeUnwrapperTraderV2ConstructorParams(
+      core,
+      core.pendleEcosystem!.wstEthJun2025.pendleRegistry,
+      core.tokens.wstEth!,
+      core.pendleEcosystem!.wstEthJun2025.dPtWstEthJun2025,
+    ),
+    'PendlePtWstEthJun2025IsolationModeUnwrapperTraderV4',
+  );
+  const wrapperV4 = await deployContractAndSave(
+    core.config.networkNumber,
+    'PendlePtIsolationModeWrapperTraderV2',
+    getPendlePtIsolationModeWrapperTraderV2ConstructorParams(
+      core,
+      core.pendleEcosystem!.wstEthJun2025.pendleRegistry,
+      core.tokens.wstEth!,
+      core.pendleEcosystem!.wstEthJun2025.dPtWstEthJun2025,
+    ),
+    'PendlePtWstEthJun2025IsolationModeWrapperTraderV4',
+  );
+  const transactions = [];
+  transactions.push(
+    await prettyPrintEncodedDataWithTypeSafety(
+      core,
+      core.pendleEcosystem!.wstEthJun2025,
+      'dPtWstEthJun2025',
+      'ownerSetIsTokenConverterTrusted',
+      [old2025Unwrapper, false],
+    ),
+  );
+  transactions.push(
+    await prettyPrintEncodedDataWithTypeSafety(
+      core,
+      core.pendleEcosystem!.wstEthJun2025,
+      'dPtWstEthJun2025',
+      'ownerSetIsTokenConverterTrusted',
+      [old2025Wrapper, false],
+    ),
+  );
+  transactions.push(
+    await prettyPrintEncodedDataWithTypeSafety(
+      core,
+      core.pendleEcosystem!.wstEthJun2025,
+      'dPtWstEthJun2025',
+      'ownerSetIsTokenConverterTrusted',
+      [unwrapperV4, true],
+    ),
+  );
+  transactions.push(
+    await prettyPrintEncodedDataWithTypeSafety(
+      core,
+      core.pendleEcosystem!.wstEthJun2025,
+      'dPtWstEthJun2025',
+      'ownerSetIsTokenConverterTrusted',
+      [wrapperV4, true],
+    ),
+  );
+  return transactions;
+}
+
 /**
  * This script encodes the following transactions:
- * - Deploys a new jUSDC Token Vault to handle auto staking if jUSDC deposit incentives are disabled
+ * - Deploys new unwrapper / wrapper contracts for PT-wstETH (Jun 2024)
+ * - Deploys new unwrapper / wrapper contracts for PT-wstETH (Jun 2025)
  */
 async function main(): Promise<DenJsonUpload> {
   const network = await getAndCheckSpecificNetwork(Network.ArbitrumOne);
   const core = await setupCoreProtocol({ network, blockNumber: 0 });
 
-  const vaultV7 = await deployContractAndSave(
-    core.config.networkNumber,
-    'JonesUSDCIsolationModeTokenVaultV2',
-    [],
-    'JonesUSDCIsolationModeTokenVaultV8',
-    core.tokenVaultActionsLibraries,
-  );
-
-  const transactions = [
-    await prettyPrintEncodedDataWithTypeSafety(
-      core,
-      core.jonesEcosystem!.live,
-      'jUSDCIsolationModeFactory',
-      'ownerSetUserVaultImplementation',
-      [vaultV7],
-    ),
+  const transactions: EncodedTransaction[] = [
+    ...await deployPtWstEthJun2024Updates(core),
+    ...await deployPtWstEthJun2025Updates(core),
   ];
 
   return {
