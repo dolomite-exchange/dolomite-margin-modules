88dd4db4f222414fbc18136e08df9c9be2e8de8e
BowTiedOriole <bowtiedoriole@protonmail.com>
2024-04-05 09:42:24 -0400

    feat: ptGlp transformer integration account

--- packages/pendle/test/Glp2024/PtGLPTransformer.ts
+++ packages/pendle/test/Glp2024/PtGLPTransformer.ts
@@ -10,6 +10,7 @@ import { BYTES_EMPTY, Network, ZERO_BI } from '@dolomite-exchange/modules-base/s
 import { revertToSnapshotAndCapture, snapshot } from '@dolomite-exchange/modules-base/test/utils';
 import { CoreProtocolArbitrumOne } from '@dolomite-exchange/modules-base/test/utils/core-protocol';
 import {
+  disableInterestAccrual,
   setupCoreProtocol,
 } from '@dolomite-exchange/modules-base/test/utils/setup';
 import { expectProtocolBalance } from '@dolomite-exchange/modules-base/test/utils/assertions';
@@ -21,10 +22,20 @@ import {
   PtGLPTransformer,
   PtGLPTransformer__factory
 } from 'packages/pendle/src/types';
+import { BigNumber } from 'ethers';
 
 const defaultAccountNumber = ZERO_BI;
 const vaultAddress = '0x10dc4c2c391de5008bc4c895c3b1c3b070661674';
+const vaultAddress2 = '0x02f78ebb68d234c0c7fe94b85de39d21d1102f6b';
 const vaultOwner = '0x9958Ed7f2441c208821Ea14643224812A006D221';
+const borrowAccount1 = BigNumber.from('4211115166896119896340262904855120100885563956626545700858695746739717416654')
+const borrowAccount2 = BigNumber.from('54588878184938659795385687626756517841802839253616459311414872509276303196718')
+
+const integrationAccounts: AccountInfoStruct[] = [
+  { owner: vaultAddress, number: defaultAccountNumber },
+  { owner: vaultAddress2, number: borrowAccount1 },
+  { owner: vaultAddress2, number: borrowAccount2 }
+];
 
 describe('PtGLPTransformer', () => {
   let snapshotId: string;
@@ -42,6 +53,8 @@ describe('PtGLPTransformer', () => {
       blockNumber: 195_821_400, // This is block prior to vault removing the tokens
       network: Network.ArbitrumOne,
     });
+    await disableInterestAccrual(core, core.marketIds.weth);
+    await disableInterestAccrual(core, core.marketIds.wbtc);
 
     ptGlpFactory = core.pendleEcosystem.glpMar2024.dPtGlp2024.connect(core.hhUser1);
     glpFactory = core.gmxEcosystem!.live.dGlp.connect(core.hhUser1);
@@ -117,6 +130,63 @@ describe('PtGLPTransformer', () => {
         core.marketIds.dfsGlp,
         ptGlpAmountWei,
       );
+    });
+
+    it('should work normally for integration accounts', async () => {
+      const ptGlpAmountWei = (await core.dolomiteMargin.getAccountWei(
+        { owner: vaultAddress, number: defaultAccountNumber },
+        core.marketIds.dPtGlp
+      )).value;
+
+      const supplyAmount = await core.dolomiteMargin.getAccountWei(
+        { owner: '0x02f78ebb68d234c0c7fe94b85de39d21d1102f6b', number: borrowAccount1 },
+        core.marketIds.dPtGlp
+      );
+      const supplyAmount2 = await core.dolomiteMargin.getAccountWei(
+        { owner: '0x02f78ebb68d234c0c7fe94b85de39d21d1102f6b', number: borrowAccount2 },
+        core.marketIds.dPtGlp
+      );
+
+      const borrowAmountWeth1 = await core.dolomiteMargin.getAccountWei(
+        { owner: vaultAddress2, number: borrowAccount1 },
+        core.marketIds.weth
+      );
+      const borrowAmountWbtc1 = await core.dolomiteMargin.getAccountWei(
+        { owner: vaultAddress2, number: borrowAccount1 },
+        core.marketIds.wbtc
+      );
+      const borrowAmountUsdc2 = await core.dolomiteMargin.getAccountWei(
+        { owner: vaultAddress2, number: borrowAccount2 },
+        core.marketIds.nativeUsdc
+      );
+      await migrator.connect(core.hhUser5).migrate(
+        integrationAccounts,
+        core.marketIds.dPtGlp,
+        core.marketIds.dfsGlp,
+        BYTES_EMPTY
+      );
+
+      const glpVaultAddress = await glpFactory.getVaultByAccount(vaultOwner);
+      const glpVaultAddress2 = await glpFactory.getVaultByAccount('0x8A8841F4AB46A052139e0DE31B1e693382193813');
+      await expectProtocolBalance(core, vaultAddress, defaultAccountNumber, core.marketIds.dPtGlp, ZERO_BI);
+      await expectProtocolBalance(
+        core,
+        glpVaultAddress,
+        defaultAccountNumber,
+        core.marketIds.dfsGlp,
+        ptGlpAmountWei,
+      );
+
+      await expectProtocolBalance(core, vaultAddress2, borrowAccount1, core.marketIds.dPtGlp, ZERO_BI);
+      await expectProtocolBalance(core, vaultAddress2, borrowAccount1, core.marketIds.wbtc, ZERO_BI);
+      await expectProtocolBalance(core, vaultAddress2, borrowAccount1, core.marketIds.weth, ZERO_BI);
+      await expectProtocolBalance(core, glpVaultAddress2, borrowAccount1, core.marketIds.wbtc, ZERO_BI.sub(borrowAmountWbtc1.value));
+      await expectProtocolBalance(core, glpVaultAddress2, borrowAccount1, core.marketIds.weth, ZERO_BI.sub(borrowAmountWeth1.value));
+      await expectProtocolBalance(core, glpVaultAddress2, borrowAccount1, core.marketIds.dfsGlp, supplyAmount.value);
+
+      await expectProtocolBalance(core, vaultAddress2, borrowAccount2, core.marketIds.dPtGlp, ZERO_BI);
+      await expectProtocolBalance(core, glpVaultAddress2, borrowAccount2, core.marketIds.dfsGlp, supplyAmount2.value);
+      await expectProtocolBalance(core, glpVaultAddress2, borrowAccount2, core.marketIds.nativeUsdc, ZERO_BI.sub(borrowAmountUsdc2.value));
     });
   });
 });
