bb173d229eb7b708dbcfd491e82013052d7f228a
bowtiedoriole <bowtiedoriole@protonmail.com>
2024-08-27 14:44:15 -0400

    feat: added test deployment script

--- packages/deployment/src/utils/dry-run-utils.ts
+++ packages/deployment/src/utils/dry-run-utils.ts
@@ -3,7 +3,6 @@ import {
   NetworkType,
   ZERO_BI,
 } from '@dolomite-exchange/modules-base/src/utils/no-deps-constants';
-import { advanceByTimeDelta, impersonate } from '@dolomite-exchange/modules-base/test/utils';
 import { CoreProtocolType } from '@dolomite-exchange/modules-base/test/utils/setup';
 import hardhat from 'hardhat';
 import { assertHardhatInvariant } from 'hardhat/internal/core/errors';
@@ -43,108 +42,6 @@ function cleanHardhatDeployment(): void {
 }
 
 async function doStuffInternal<T extends NetworkType>(executionFn: () => Promise<DryRunOutput<T>>) {
-  if (hardhat.network.name === 'hardhat') {
-    const result = await executionFn();
-
-    if (result.core) {
-      console.log('\tSimulating admin transactions...');
-      const signer = await impersonate((await result.core.delayedMultiSig.getOwners())[0], true);
-      const delayedMultiSig = result.core.delayedMultiSig.connect(signer);
-      const filter = delayedMultiSig.filters.Submission();
-      const transactionIds = [];
-      const timeDelay = result.skipTimeDelay ? 0 : await delayedMultiSig.secondsTimeLocked();
-
-      if (result.skipTimeDelay) {
-        console.log('\tSkipping time delay...');
-        const impersonator = await impersonate(delayedMultiSig.address, true);
-        await delayedMultiSig.connect(impersonator).changeTimeLock(0);
-      }
-
-      for (const transaction of result.upload.transactions) {
-        let txResult;
-        if (transaction.to === result.core.delayedMultiSig.address) {
-          txResult = await signer.sendTransaction({
-            to: transaction.to,
-            data: transaction.data,
-            from: signer.address,
-          });
-        } else {
-          txResult = await delayedMultiSig.submitTransaction(transaction.to, ZERO_BI, transaction.data);
-        }
-
-        const submissionEvent = (await delayedMultiSig.queryFilter(filter, txResult.blockHash))[0];
-        if (submissionEvent) {
-          transactionIds.push(submissionEvent.args.transactionId);
-        }
-      }
-
-      console.log('\tSubmitted transactions. Advancing time forward...');
-      await advanceByTimeDelta(timeDelay + 1);
-
-      console.log('\tExecuting transactions...');
-      for (const transactionId of transactionIds) {
-        try {
-          hardhat.tracer.enabled = true;
-          await delayedMultiSig.executeMultipleTransactions([transactionId], {});
-          hardhat.tracer.enabled = false;
-        } catch (e: any) {
-          const transactionIndex = transactionId.sub(transactionIds[0]).toNumber();
-          throw new Error(
-            `Execution of transaction with ID ${transactionId.toString()} failed due to error: ${e.message}\n
-            transaction: ${JSON.stringify(result.upload.transactions[transactionIndex], undefined, 2)}`,
-          );
-        }
-      }
-      console.log('\tAdmin transactions succeeded!');
-    }
-
-    if (result.invariants) {
-      console.log('\tChecking invariants...');
-      await result.invariants();
-      console.log('\tInvariants passed!');
-    } else {
-      console.log('\tNo invariants found, skipping...');
-    }
-  } else {
-    return executionFn().then(async (result) => {
-      if (typeof result === 'undefined') {
-        return;
-      }
-
-      if (result.upload.addExecuteImmediatelyTransactions && result.upload.transactions.length > 0) {
-        let transactionCount = (await result.core.delayedMultiSig.transactionCount()).toNumber();
-        const submitTransactionMethodId = '0xc6427474';
-        const transactionIds: number[] = [];
-        result.upload.transactions.forEach((t) => {
-          if (t.data.startsWith(submitTransactionMethodId)) {
-            transactionIds.push(transactionCount++);
-          }
-        });
-
-        assertHardhatInvariant(transactionIds.length > 0, 'Transaction IDs length must be greater than 0');
-
-        result.upload.transactions.push(
-          await prettyPrintEncodedDataWithTypeSafety(
-            result.core,
-            { delayedMultisig: result.core.delayedMultiSig },
-            'delayedMultisig',
-            'executeMultipleTransactions',
-            [transactionIds],
-            { skipWrappingCalldataInSubmitTransaction: true },
-          ),
-        );
-      }
-
-      const scriptName = result.scriptName;
-      const networkName = networkToNetworkNameMap[result.upload.chainId];
-      const dir = `${__dirname}/../deploy/safe-transactions/${networkName}/output`;
-      createFolder(dir);
-      writeFile(`${dir}/${scriptName}.json`, JSON.stringify(result.upload, null, 2));
-    });
-  }
-}
-
-async function doStuffInternalWithDolomiteOwner<T extends NetworkType>(executionFn: () => Promise<DryRunOutput<T>>) {
   if (hardhat.network.name === 'hardhat') {
     const result = await executionFn();
 
