ee0fec4e12f991c467716205e8a26ae99a012aca
Corey <corey@dolomite.io>
2024-10-21 02:58:26 -0400

    update bera scripts

---
+++ packages/deployment/src/deploy/safe-transactions/berachain/safe-transaction-998.ts
@@ -0,0 +1,81 @@
+import { IERC20, TestPriceOracle__factory } from '@dolomite-exchange/modules-base/src/types';
+import { getAndCheckSpecificNetwork } from '@dolomite-exchange/modules-base/src/utils/dolomite-utils';
+import { getRealLatestBlockNumber } from '@dolomite-exchange/modules-base/test/utils';
+import { CoreProtocolBerachain } from '@dolomite-exchange/modules-base/test/utils/core-protocols/core-protocol-berachain';
+import { setupCoreProtocol } from '@dolomite-exchange/modules-base/test/utils/setup';
+import { BigNumberish } from 'ethers';
+import { Network } from 'packages/base/src/utils/no-deps-constants';
+import { EncodedTransaction, prettyPrintEncodedDataWithTypeSafety } from '../../../utils/deploy-utils';
+import { doDryRunAndCheckDeployment, DryRunOutput } from '../../../utils/dry-run-utils';
+import getScriptName from '../../../utils/get-script-name';
+import ModuleDeployments from '../../deployments.json';
+
+async function encodeTestOracle(
+  token: IERC20,
+  price: BigNumberish,
+  core: CoreProtocolBerachain,
+): Promise<EncodedTransaction[]> {
+  const testPriceOracle = TestPriceOracle__factory.connect(
+    ModuleDeployments.TestPriceOracle['80084'].address,
+    core.hhUser1,
+  );
+
+  return [
+    await prettyPrintEncodedDataWithTypeSafety(core, { testPriceOracle }, 'testPriceOracle', 'setPrice', [
+      token.address,
+      price,
+    ]),
+  ];
+}
+
+/**
+ * This script encodes the following transactions:
+ * - Sets WETH's price oracle to the test oracle
+ */
+async function main(): Promise<DryRunOutput<Network.Berachain>> {
+  const network = await getAndCheckSpecificNetwork(Network.Berachain);
+  const core = await setupCoreProtocol({
+    network,
+    blockNumber: await getRealLatestBlockNumber(true, network),
+  });
+
+  const transactions: EncodedTransaction[] = [];
+
+  if (network === '80084') {
+    const testPriceOracle = TestPriceOracle__factory.connect(
+      ModuleDeployments.TestPriceOracle['80084'].address,
+      core.hhUser1,
+    );
+
+    transactions.push(await prettyPrintEncodedDataWithTypeSafety(
+      core,
+      { dolomite: core.dolomiteMargin },
+      'dolomite',
+      'ownerSetPriceOracle',
+      [core.marketIds.weth, testPriceOracle.address],
+    ));
+  }
+
+  return {
+    core,
+    upload: {
+      transactions,
+      addExecuteImmediatelyTransactions: true,
+      chainId: core.network,
+      version: '1.0',
+      meta: {
+        txBuilderVersion: '1.16.5',
+        name: __filename,
+      },
+    },
+    scriptName: getScriptName(__filename),
+    invariants: async () => {
+      console.log(
+        '\t Price for weth',
+        (await core.dolomiteMargin.getMarketPrice(core.marketIds.weth)).value.toString(),
+      );
+    },
+  };
+}
+
+doDryRunAndCheckDeployment(main);
