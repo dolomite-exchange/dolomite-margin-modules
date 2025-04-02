d374b77840b0a0a6284de9ed27884158b25e23c7
scyron6 <stephencyron@gmail.com>
2025-04-02 09:25:11 -0400

    feat: bare setup of admin claim contract

---
+++ packages/admin/contracts/AdminClaimExcessTokens.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+
+    Copyright 2025 Dolomite
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+pragma solidity ^0.8.9;
+
+import { Require } from "@dolomite-exchange/modules-base/contracts/protocol/lib/Require.sol";
+import { AccessControl } from "@openzeppelin/contracts/access/AccessControl.sol";
+import { IAccessControl } from "@openzeppelin/contracts/access/IAccessControl.sol";
+import { Address } from "@openzeppelin/contracts/utils/Address.sol";
+import { EnumerableSet } from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import { IDolomiteOwner } from "./interfaces/IDolomiteOwner.sol";
+
+
+/**
+ * @title   AdminClaimExcessTokens
+ * @author  Dolomite
+ *
+ * @notice  AdminClaimExcessTokens contract that enables an admin to claim excess tokens from the protocol
+ */
+contract AdminClaimExcessTokens {
+}
