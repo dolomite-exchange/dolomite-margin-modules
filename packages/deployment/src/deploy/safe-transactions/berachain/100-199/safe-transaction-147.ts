9f48aaec66147fa5ae70aa1c6d351e21d3f51d29
Corey <coreycaplan3@gmail.com>
2025-04-10 15:19:05 -0400

    update script for 147

--- packages/deployment/src/deploy/safe-transactions/berachain/100-199/safe-transaction-147.ts
+++ packages/deployment/src/deploy/safe-transactions/berachain/100-199/safe-transaction-147.ts
@@ -2,19 +2,32 @@ import { getAndCheckSpecificNetwork } from '@dolomite-exchange/modules-base/src/
 import { Network } from '@dolomite-exchange/modules-base/src/utils/no-deps-constants';
 import { getRealLatestBlockNumber } from '@dolomite-exchange/modules-base/test/utils';
 import { setupCoreProtocol } from '@dolomite-exchange/modules-base/test/utils/setup';
-import { parseEther } from 'ethers/lib/utils';
 import { doDryRunAndCheckDeployment, DryRunOutput, EncodedTransaction } from '../../../../utils/dry-run-utils';
 import {
   encodeSetBorrowCapWithMagic,
-  encodeSetInterestSetter, encodeSetIsBorrowOnly,
+  encodeSetIsBorrowOnly,
   encodeSetIsCollateralOnly,
+  encodeSetLiquidationPenalty,
+  encodeSetMinCollateralization,
+  encodeSetSupplyCapWithMagic,
 } from '../../../../utils/encoding/dolomite-margin-core-encoder-utils';
 import getScriptName from '../../../../utils/get-script-name';
-import { checkBorrowCap, checkInterestSetter, checkIsCollateralOnly } from '../../../../utils/invariant-utils';
+import {
+  TargetCollateralization,
+  TargetLiquidationPenalty,
+} from '@dolomite-exchange/modules-base/src/utils/constructors/dolomite';
+import {
+  checkAccountRiskOverrideIsBorrowOnly, checkBorrowCap,
+  checkIsCollateralOnly, checkLiquidationPenalty, checkMinCollateralization,
+  checkSupplyCap,
+} from '../../../../utils/invariant-utils';
+import { parseEther } from 'ethers/lib/utils';
 
 /**
  * This script encodes the following transactions:
  * - Delist stBTC
+ * - Update risk for SBTC, ylStETH
+ * - Update caps for rsETH and rswETH
  */
 async function main(): Promise<DryRunOutput<Network.Berachain>> {
   const network = await getAndCheckSpecificNetwork(Network.Berachain);
@@ -26,6 +39,23 @@ async function main(): Promise<DryRunOutput<Network.Berachain>> {
   const transactions: EncodedTransaction[] = [
     await encodeSetIsBorrowOnly(core, core.marketIds.stBtc, true),
     await encodeSetIsCollateralOnly(core, core.marketIds.stBtc, true),
+
+    await encodeSetIsBorrowOnly(core, core.marketIds.ylFbtc, true),
+    await encodeSetIsCollateralOnly(core, core.marketIds.ylFbtc, true),
+
+    await encodeSetIsBorrowOnly(core, core.marketIds.ylStEth, true),
+    await encodeSetIsCollateralOnly(core, core.marketIds.ylStEth, true),
+
+    await encodeSetSupplyCapWithMagic(core, core.marketIds.sbtc, 25),
+    await encodeSetBorrowCapWithMagic(core, core.marketIds.sbtc, 10),
+    await encodeSetMinCollateralization(core, core.marketIds.sbtc, TargetCollateralization._133),
+    await encodeSetLiquidationPenalty(core, core.marketIds.sbtc, TargetLiquidationPenalty._9),
+
+    await encodeSetSupplyCapWithMagic(core, core.marketIds.rsEth, 1),
+    await encodeSetSupplyCapWithMagic(core, core.marketIds.rswEth, 1),
+
+    await encodeSetMinCollateralization(core, core.marketIds.ylStEth, TargetCollateralization._150),
+    await encodeSetLiquidationPenalty(core, core.marketIds.ylStEth, TargetLiquidationPenalty._15),
   ];
 
   return {
@@ -41,7 +71,28 @@ async function main(): Promise<DryRunOutput<Network.Berachain>> {
       },
     },
     scriptName: getScriptName(__filename),
-    invariants: async () => {},
+    invariants: async () => {
+      await checkAccountRiskOverrideIsBorrowOnly(core, core.marketIds.stBtc);
+      await checkIsCollateralOnly(core, core.marketIds.stBtc, true);
+
+      await checkAccountRiskOverrideIsBorrowOnly(core, core.marketIds.ylFbtc);
+      await checkIsCollateralOnly(core, core.marketIds.ylFbtc, true);
+
+      await checkAccountRiskOverrideIsBorrowOnly(core, core.marketIds.ylStEth);
+      await checkIsCollateralOnly(core, core.marketIds.ylStEth, true);
+
+      await checkSupplyCap(core, core.marketIds.sbtc, parseEther(`${25}`));
+      await checkBorrowCap(core, core.marketIds.sbtc, parseEther(`${10}`));
+      await checkMinCollateralization(core, core.marketIds.sbtc, TargetCollateralization._133);
+      await checkLiquidationPenalty(core, core.marketIds.sbtc, TargetLiquidationPenalty._9);
+
+      await checkSupplyCap(core, core.marketIds.rsEth, parseEther(`${1}`));
+
+      await checkSupplyCap(core, core.marketIds.rswEth, parseEther(`${1}`));
+
+      await checkMinCollateralization(core, core.marketIds.ylStEth, TargetCollateralization._150);
+      await checkLiquidationPenalty(core, core.marketIds.ylStEth, TargetLiquidationPenalty._15);
+    },
   };
 }
 
