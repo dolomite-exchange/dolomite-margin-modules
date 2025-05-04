e52da15ec3a1e9aa9183afdc92c589e65d8844da
Corey <corey@dolomite.io>
2025-05-04 10:15:07 -0400

    xfer

---
+++ packages/deployment/src/deploy/safe-transactions/arbitrum_one/500-599/safe-transaction-535.ts
@@ -0,0 +1,44 @@
+import { getAndCheckSpecificNetwork } from '@dolomite-exchange/modules-base/src/utils/dolomite-utils';
+import { Network } from '@dolomite-exchange/modules-base/src/utils/no-deps-constants';
+import { getRealLatestBlockNumber } from '@dolomite-exchange/modules-base/test/utils';
+import { setupCoreProtocol } from '@dolomite-exchange/modules-base/test/utils/setup';
+import { doDryRunAndCheckDeployment, DryRunOutput, EncodedTransaction } from '../../../../utils/dry-run-utils';
+import { prettyPrintEncodedDataWithTypeSafety } from '../../../../utils/encoding/base-encoder-utils';
+import getScriptName from '../../../../utils/get-script-name';
+
+/**
+ * This script encodes the following transactions:
+ * - Disables GRAI borrowing
+ */
+async function main(): Promise<DryRunOutput<Network.ArbitrumOne>> {
+  const network = await getAndCheckSpecificNetwork(Network.ArbitrumOne);
+  const core = await setupCoreProtocol({ network, blockNumber: await getRealLatestBlockNumber(true, network) });
+
+  const transactions: EncodedTransaction[] = [
+    await prettyPrintEncodedDataWithTypeSafety(
+      core,
+      { dolomite: core.dolomiteMargin },
+      'dolomite',
+      'ownerSetIsClosing',
+      [core.marketIds.grai, true],
+    ),
+  ];
+
+  return {
+    core,
+    scriptName: getScriptName(__filename),
+    upload: {
+      transactions,
+      chainId: network,
+      addExecuteImmediatelyTransactions: true,
+      version: '1.0',
+      meta: {
+        txBuilderVersion: '1.16.5',
+        name: __filename,
+      },
+    },
+    invariants: async () => {},
+  };
+}
+
+doDryRunAndCheckDeployment(main);
