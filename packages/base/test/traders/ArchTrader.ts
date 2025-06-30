75f8338a03df4ff9490f869ea28694e9168ad0c6
scyron6 <stephencyron@gmail.com>
2025-06-30 10:15:12 -0400

    feat: arch trader botanix test setup

--- packages/base/test/traders/ArchTrader.ts
+++ packages/base/test/traders/ArchTrader.ts
@@ -1,11 +1,5 @@
-import { ActionType, AmountDenomination, AmountReference } from '@dolomite-margin/dist/src';
-import { expect } from 'chai';
 import { BigNumber } from 'ethers';
-import { ethers } from 'hardhat';
-import { OdosAggregatorTrader } from '../../src/types';
-import { AccountStruct } from '../../src/utils/constants';
-import { depositIntoDolomiteMargin } from '../../src/utils/dolomite-utils';
-import { BYTES_EMPTY, Network, ZERO_BI } from '../../src/utils/no-deps-constants';
+import { Network } from '../../src/utils/no-deps-constants';
 import { getRealLatestBlockNumber, revertToSnapshotAndCapture, snapshot } from '../utils';
 import { expectProtocolBalance, expectProtocolBalanceIsGreaterThan, expectThrow } from '../utils/assertions';
 
@@ -13,6 +7,7 @@ import { CoreProtocolArbitrumOne } from '../utils/core-protocols/core-protocol-a
 import { createOdosAggregatorTrader } from '../utils/ecosystem-utils/traders';
 import { disableInterestAccrual, setupCoreProtocol, setupWETHBalance } from '../utils/setup';
 import { getCalldataForOdos } from '../utils/trader-utils';
+import { CoreProtocolBotanix } from '../utils/core-protocols/core-protocol-botanix';
 
 const defaultAccountNumber = '0';
 const amountIn = BigNumber.from('1000000000000000000');
@@ -21,7 +16,7 @@ const minAmountOut = BigNumber.from('123123123');
 describe('ArchTrader', () => {
   let snapshotId: string;
 
-  let core: CoreProtocolArbitrumOne;
+  let core: CoreProtocolBotanix;
 
   before(async () => {
     const latestBlockNumber = await getRealLatestBlockNumber(true, Network.Botanix);
