1607b9361bf2c72b0ae6ec6e16ecb17a7b8f608b
scyron6 <stephencyron@gmail.com>
2025-07-30 11:23:10 -0400

    feat: created admin registry and helper contracts

---
+++ packages/admin/contracts/AdminRegistry.sol
@@ -0,0 +1,88 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+
+    Copyright 2025 Dolomite
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+pragma solidity ^0.8.9;
+
+import { OnlyDolomiteMargin } from "@dolomite-exchange/modules-base/contracts/helpers/OnlyDolomiteMargin.sol";
+import { IDolomiteRegistry } from "@dolomite-exchange/modules-base/contracts/interfaces/IDolomiteRegistry.sol";
+import { IDolomiteMarginAdmin } from "@dolomite-exchange/modules-base/contracts/protocol/interfaces/IDolomiteMarginAdmin.sol"; // solhint-disable-line max-line-length
+import { Require } from "@dolomite-exchange/modules-base/contracts/protocol/lib/Require.sol";
+import { IModularLinearStepFunctionInterestSetter } from "@dolomite-exchange/modules-interest-setters/contracts/interfaces/IModularLinearStepFunctionInterestSetter.sol"; // solhint-disable-line max-line-length
+import { IAdminSetInterestSetter } from "./interfaces/IAdminSetInterestSetter.sol";
+import { IDolomiteOwner } from "./interfaces/IDolomiteOwner.sol";
+import { AccessControl } from "@openzeppelin/contracts/access/AccessControl.sol";
+import { IAdminRegistry } from "./interfaces/IAdminRegistry.sol";
+
+
+/**
+ * @title   AdminRegistry
+ * @author  Dolomite
+ *
+ * @notice  AdminRegistry contract to track roles and permissions across Dolomite admin contracts
+ */
+contract AdminRegistry is AccessControl, OnlyDolomiteMargin, IAdminRegistry {
+
+    // ===================================================================
+    // ============================ Constants ============================
+    // ===================================================================
+
+    bytes32 private constant _FILE = "AdminRegistry";
+
+    // ===================================================================
+    // ========================== Constructor ============================
+    // ===================================================================
+
+    constructor(
+        address _dolomiteMargin
+    ) OnlyDolomiteMargin(_dolomiteMargin) {
+    }
+
+    // ===================================================================
+    // ========================= Admin Functions =========================
+    // ===================================================================
+
+    function grantRole(
+        bytes4 _selector,
+        address _contract,
+        address _account
+    ) external onlyDolomiteMarginOwner(msg.sender) {
+        _grantRole(role(_selector, _contract), _account);
+    }
+
+    function revokeRole(
+        bytes4 _selector,
+        address _contract,
+        address _account
+    ) external onlyDolomiteMarginOwner(msg.sender) {
+        _revokeRole(role(_selector, _contract), _account);
+    }
+
+    // ===================================================================
+    // ========================= View Functions ==========================
+    // ===================================================================
+
+    function role(bytes4 _selector, address _contract) public pure returns (bytes32) {
+        return bytes32(_selector) | bytes32(uint256(uint160(_contract)));
+    }
+
+    function hasPermission(bytes4 _selector, address _contract, address _account) external view returns (bool) {
+        return hasRole(role(_selector, _contract), _account);
+    }
+}
---
+++ packages/admin/contracts/AdminRegistryHelper.sol
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+
+    Copyright 2025 Dolomite
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+pragma solidity ^0.8.9;
+
+import { IDolomiteRegistry } from "@dolomite-exchange/modules-base/contracts/interfaces/IDolomiteRegistry.sol";
+import { Require } from "@dolomite-exchange/modules-base/contracts/protocol/lib/Require.sol";
+import { IAdminRegistry } from "./interfaces/IAdminRegistry.sol";
+
+
+/**
+ * @title   AdminRegistryHelper
+ * @author  Dolomite
+ *
+ * @notice  AdminRegistryHelper contract to store AdminRegistry and check permissions
+ */
+abstract contract AdminRegistryHelper {
+
+    // ===================================================================
+    // ============================ Constants ============================
+    // ===================================================================
+
+    bytes32 private constant _FILE = "AdminRegistryHelper";
+
+    // ===================================================================
+    // ======================= Field Variables ===========================
+    // ===================================================================
+
+    IDolomiteRegistry public immutable DOLOMITE_REGISTRY;
+
+    // ===================================================================
+    // ========================== Constructor ============================
+    // ===================================================================
+
+    constructor(
+        address _dolomiteRegistry
+    ) {
+        DOLOMITE_REGISTRY = IDolomiteRegistry(_dolomiteRegistry);
+    }
+
+    // ===================================================================
+    // ========================== Modifiers =============================
+    // ===================================================================
+
+    modifier checkPermission(bytes4 _selector) {
+        _checkPermission(_selector);
+        _;
+    }
+
+    // ===================================================================
+    // ========================= Internal Functions ======================
+    // ===================================================================
+
+    function _checkPermission(bytes4 _selector) internal view {
+        IAdminRegistry adminRegistry = IAdminRegistry(DOLOMITE_REGISTRY.adminRegistry());
+        Require.that(
+            adminRegistry.hasPermission(_selector, address(this), msg.sender),
+            _FILE,
+            "Caller does not have permission"
+        );
+    }
+}
