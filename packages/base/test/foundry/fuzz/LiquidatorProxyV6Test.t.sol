f9589096ccde3b126ecb0eee57ec915fecb68e6b
scyron6 <stephencyron@gmail.com>
2025-11-17 13:20:20 -0500

    feat: fuzz test for liquidation with no rake

--- packages/base/test/foundry/fuzz/LiquidatorProxyV6Test.t.sol
+++ packages/base/test/foundry/fuzz/LiquidatorProxyV6Test.t.sol
@@ -66,6 +66,96 @@ contract LiquidatorProxyV6Test is DolomiteForkTest {
     // =============== Liquidate Tests ===============
     // =============================================
 
+    function test_liquidate_lossy_no_rake() public {
+        vm.prank(dolomiteOwner);
+        liquidatorProxy.ownerSetDolomiteRake(IDolomiteStructs.Decimal({
+            value: 0
+        }));
+
+        uint256 amountWei = 1000 ether;
+        deal(address(dai), alice, amountWei);
+
+        vm.startPrank(alice);
+        dai.approve(address(depositWithdrawalRouter), amountWei);
+        depositWithdrawalRouter.depositWei(
+            0,
+            BORROW_ACCOUNT_NUMBER,
+            daiMarketId,
+            amountWei,
+            IDepositWithdrawalRouter.EventFlag.None
+        );
+        assertProtocolBalanceWei(dolomiteMargin, alice, BORROW_ACCOUNT_NUMBER, daiMarketId, amountWei, true);
+        console2.log("succesfully did alice deposit");
+
+        borrowPositionProxy.transferBetweenAccounts(
+            BORROW_ACCOUNT_NUMBER,
+            DEFAULT_ACCOUNT_NUMBER,
+            wethMarketId,
+            1 ether,
+            AccountBalanceLib.BalanceCheckFlag.None
+        );
+
+        vm.stopPrank();
+
+        constantPriceOracle.setPrice(address(weth), 900 ether); // $900
+
+        uint256 minOutputAmountWei = 1.1 ether;
+        IGenericTraderBase.TraderParam[] memory tradersPath = new IGenericTraderBase.TraderParam[](1);
+        tradersPath[0] = IGenericTraderBase.TraderParam({
+            traderType: IGenericTraderBase.TraderType.ExternalLiquidity,
+            makerAccountIndex: 0,
+            trader: address(dolomiteExchangeWrapper),
+            tradeData: abi.encode(minOutputAmountWei, abi.encode(minOutputAmountWei))
+        });
+        uint256[] memory marketIdsPath = new uint256[](2);
+        marketIdsPath[0] = daiMarketId;
+        marketIdsPath[1] = wethMarketId;
+
+        vm.prank(bob);
+        liquidatorProxy.liquidate(ILiquidatorProxyV6.LiquidateParams({
+            solidAccount: IDolomiteStructs.AccountInfo({
+                owner: bob,
+                number: DEFAULT_ACCOUNT_NUMBER
+            }),
+            liquidAccount: IDolomiteStructs.AccountInfo({
+                owner: alice,
+                number: BORROW_ACCOUNT_NUMBER
+            }),
+            marketIdsPath: marketIdsPath,
+            inputAmountWei: type(uint256).max,
+            minOutputAmountWei: type(uint256).max,
+            tradersPath: tradersPath,
+            makerAccounts: new IDolomiteStructs.AccountInfo[](0),
+            expirationTimestamp: 0,
+            withdrawAllReward: false
+        }));
+
+        /*
+        * held = 1000 DAI
+        * owed = 1 WETH
+        * heldPrice = $1
+        * owedPrice = $900
+        * owedPriceAdj = 900 + .05(900) = $945
+        *
+        * After liquidation action:
+        *     liquid account dai = 1000 - 945 = 55
+        *     liquid account weth = 1 - 1 = 0
+        *     solid account dai = 945
+        *     solid account weth = -1
+        *     dolomite rake account dai = 0
+        *
+        * After trade action where solid account swaps 945 dai for 1.1 weth:
+        *     solid account dai = 0
+        *     solid account weth = .1
+        */
+        assertProtocolBalanceWei(dolomiteMargin, alice, BORROW_ACCOUNT_NUMBER, wethMarketId, 0, true);
+        assertProtocolBalanceWei(dolomiteMargin, alice, BORROW_ACCOUNT_NUMBER, daiMarketId, 55 ether, true);
+        assertProtocolBalanceWei(dolomiteMargin, bob, DEFAULT_ACCOUNT_NUMBER, wethMarketId, .1 ether, true);
+        assertProtocolBalanceWei(dolomiteMargin, bob, DEFAULT_ACCOUNT_NUMBER, daiMarketId, 0, false);
+        assertProtocolBalanceWei(dolomiteMargin, charlie, DEFAULT_ACCOUNT_NUMBER, daiMarketId, 0, true);
+        assertEq(dai.balanceOf(address(dolomiteExchangeWrapper)), 945 ether);
+    }
+
     function test_liquidate_lossy_10_percent_rake() public {
         uint256 amountWei = 1000 ether;
         deal(address(dai), alice, amountWei);
@@ -144,16 +234,10 @@ contract LiquidatorProxyV6Test is DolomiteForkTest {
         *     solid account weth = .1
         */
         assertProtocolBalanceWei(dolomiteMargin, alice, BORROW_ACCOUNT_NUMBER, wethMarketId, 0, true);
-        console2.log("here1");
         assertProtocolBalanceWei(dolomiteMargin, alice, BORROW_ACCOUNT_NUMBER, daiMarketId, 55 ether, true);
-        console2.log("here2");
         assertProtocolBalanceWei(dolomiteMargin, bob, DEFAULT_ACCOUNT_NUMBER, wethMarketId, .1 ether, true);
-        console2.log("here3");
         assertProtocolBalanceWei(dolomiteMargin, bob, DEFAULT_ACCOUNT_NUMBER, daiMarketId, 0, false);
-        console2.log("here4");
         assertProtocolBalanceWei(dolomiteMargin, charlie, DEFAULT_ACCOUNT_NUMBER, daiMarketId, 4.5 ether, true);
-        console2.log("here5");
         assertEq(dai.balanceOf(address(dolomiteExchangeWrapper)), 940.5 ether);
-        console2.log("here6");
     }
 }
