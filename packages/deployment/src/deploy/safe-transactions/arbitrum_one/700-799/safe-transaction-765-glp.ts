a201b7a67b06e9d658724365362870b9487fef12
Corey <corey@dolomite.io>
2025-12-19 11:20:19 -0500

    updated script again

--- packages/deployment/src/deploy/safe-transactions/arbitrum_one/700-799/safe-transaction-765-glp.ts
+++ packages/deployment/src/deploy/safe-transactions/arbitrum_one/700-799/safe-transaction-765-glp.ts
@@ -1,19 +1,19 @@
 import { getAndCheckSpecificNetwork } from '@dolomite-exchange/modules-base/src/utils/dolomite-utils';
 import { Network, ONE_BI } from '@dolomite-exchange/modules-base/src/utils/no-deps-constants';
-import { getRealLatestBlockNumber, impersonate } from '@dolomite-exchange/modules-base/test/utils';
+import { getRealLatestBlockNumber } from '@dolomite-exchange/modules-base/test/utils';
 import { setupCoreProtocol } from '@dolomite-exchange/modules-base/test/utils/setup';
-import { getMaxDeploymentVersionAddressByDeploymentKey } from 'packages/deployment/src/utils/deploy-utils';
-import { prettyPrintEncodedDataWithTypeSafety } from 'packages/deployment/src/utils/encoding/base-encoder-utils';
+import { readFileSync } from 'fs';
+import { writeFile } from 'fs-extra';
+import { GLPRedemptionOperator__factory } from 'packages/glp/src/types';
 import { ModuleDeployments } from '../../../../utils';
 import { doDryRunAndCheckDeployment, DryRunOutput, EncodedTransaction } from '../../../../utils/dry-run-utils';
 import getScriptName from '../../../../utils/get-script-name';
-import { readFileSync } from 'fs';
-import { GLPRedemptionOperator__factory } from 'packages/glp/src/types';
 
 const UNEXECUTED_USERS_PATH = `${__dirname}/../../../../../../glp/unexecuted-users.json`;
 const HANDLER_ADDRESS = '0x1fF6B8E1192eB0369006Bbad76dA9068B68961B2';
+
 /**
- * Executes GLP redemption for users with no margin accounts 
+ * Executes GLP redemption for users with no margin accounts
  */
 async function main(): Promise<DryRunOutput<Network.ArbitrumOne>> {
   const network = await getAndCheckSpecificNetwork(Network.ArbitrumOne);
@@ -28,8 +28,11 @@ async function main(): Promise<DryRunOutput<Network.ArbitrumOne>> {
     core.hhUser1,
   );
 
-  const handlerImpersonator = await impersonate(HANDLER_ADDRESS, true);
   for (const user of allUsers) {
+    if (user.isExecuted) {
+      continue;
+    }
+
     const redemptionParams = [];
     redemptionParams.push({
       accountNumber: '0',
@@ -37,7 +40,11 @@ async function main(): Promise<DryRunOutput<Network.ArbitrumOne>> {
       minOutputAmountWei: ONE_BI,
     });
     console.log(`Executing redemption for ${user['user']}`);
-    await redemptionOperator.connect(handlerImpersonator).handlerExecuteVault(user['vaultAddress'], redemptionParams);
+    await redemptionOperator
+      .connect(core.hhUser1)
+      .handlerExecuteVault(user['vaultAddress'], redemptionParams, { gasLimit: 10_000_000 });
+    user.isExecuted = true;
+    await writeFile(UNEXECUTED_USERS_PATH, JSON.stringify(allUsers, null, 2));
     console.log(`Executed redemption for ${user['user']}`);
     console.log('');
   }
