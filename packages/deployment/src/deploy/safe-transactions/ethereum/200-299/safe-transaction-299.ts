5733db645ef21f4bc4fb4847a96b4275bf88bf57
scyron6 <stephencyron@gmail.com>
2026-04-06 12:37:16 -0400

    feat: invariants on safe transaction

--- packages/deployment/src/deploy/safe-transactions/ethereum/200-299/safe-transaction-299.ts
+++ packages/deployment/src/deploy/safe-transactions/ethereum/200-299/safe-transaction-299.ts
@@ -1,12 +1,10 @@
-import { parseEther } from 'ethers/lib/utils';
-import { AdminSetRiskParams__factory, PartnerClaimExcessTokens__factory } from 'packages/admin/src/types';
+import { AdminSetRiskParams__factory } from 'packages/admin/src/types';
 import { getAndCheckSpecificNetwork } from '../../../../../../base/src/utils/dolomite-utils';
-import { Network } from '../../../../../../base/src/utils/no-deps-constants';
-import { getRealLatestBlockNumber, impersonate } from '../../../../../../base/test/utils';
+import { Network, ONE_BI } from '../../../../../../base/src/utils/no-deps-constants';
+import { getRealLatestBlockNumber } from '../../../../../../base/test/utils';
 import { setupCoreProtocol } from '../../../../../../base/test/utils/setup';
 import { deployContractAndSave } from '../../../../utils/deploy-utils';
 import { doDryRunAndCheckDeployment, DryRunOutput, EncodedTransaction } from '../../../../utils/dry-run-utils';
-import { encodeSetGlobalOperatorIfNecessary } from '../../../../utils/encoding/dolomite-margin-core-encoder-utils';
 import {
   ALL_FUNCTIONS,
   encodeAddressForRole,
@@ -16,6 +14,20 @@ import {
   encodeGrantRoleIfNecessary,
 } from '../../../../utils/encoding/dolomite-owner-encoder-utils';
 import getScriptName from '../../../../utils/get-script-name';
+import { assertHardhatInvariant } from 'hardhat/internal/core/errors';
+import { keccak256, toUtf8Bytes } from 'ethers/lib/utils';
+import { BYTES_EMPTY } from '@dolomite-exchange/zap-sdk/dist/src/lib/Constants';
+import { expect } from 'chai';
+
+const SET_CATEGORIES_BY_MARKET_IDS_SELECTOR = keccak256(toUtf8Bytes('setCategoriesByMarketIds(uint256[],uint8[])')).slice(0, 10); // tslint-disable-line
+const SET_CATEGORY_BY_MARKET_ID_SELECTOR = keccak256(toUtf8Bytes('setCategoryByMarketId(uint256,uint8)')).slice(0, 10);
+const SET_CATEGORY_PARAM_SELECTOR = keccak256(toUtf8Bytes('setCategoryParam(uint8,(uint256),(uint256))')).slice(0, 10);
+const SET_RISK_FEATURE_BY_MARKET_ID_SELECTOR = keccak256(toUtf8Bytes('setRiskFeatureByMarketId(uint256,uint8,bytes)')).slice(0, 10); // tslint-disable-line
+
+const OWNER_SET_MAX_SUPPLY_WEI_SELECTOR = keccak256(toUtf8Bytes('ownerSetMaxSupplyWei(uint256,uint256)')).slice(0, 10);
+const OWNER_SET_MAX_BORROW_WEI_SELECTOR = keccak256(toUtf8Bytes('ownerSetMaxBorrowWei(uint256,uint256)')).slice(0, 10);
+const OWNER_SET_MARGIN_PREMIUM_SELECTOR = keccak256(toUtf8Bytes('ownerSetMarginPremium(uint256,(uint256))')).slice(0, 10); // tslint-disable-line
+const OWNER_SET_LIQUIDATION_SPREAD_PREMIUM_SELECTOR = keccak256(toUtf8Bytes('ownerSetLiquidationSpreadPremium(uint256,(uint256))')).slice(0, 10); // tslint-disable-line
 
 /**
  * This script encodes the following transactions:
@@ -94,6 +106,101 @@ async function main(): Promise<DryRunOutput<Network.Ethereum>> {
     },
     scriptName: getScriptName(__filename),
     invariants: async () => {
+      const setMaxSupplyData = await core.dolomiteMargin.populateTransaction.ownerSetMaxSupplyWei(core.marketIds.usd1, ONE_BI);
+      assertHardhatInvariant(
+        await core.ownerAdapterV2.isUserApprovedToSubmitTransaction(
+          adminSetRiskParams.address,
+          core.dolomiteMargin.address,
+          setMaxSupplyData.data!.slice(0,10)
+        ),
+        'Admin risk setter is not approved for setting max supply wei'
+      );
+
+      const setMaxBorrowData = await core.dolomiteMargin.populateTransaction.ownerSetMaxBorrowWei(core.marketIds.usd1, ONE_BI);
+      assertHardhatInvariant(
+        await core.ownerAdapterV2.isUserApprovedToSubmitTransaction(
+          adminSetRiskParams.address,
+          core.dolomiteMargin.address,
+          setMaxBorrowData.data!.slice(0,10)
+        ),
+        'Admin risk setter is not approved for setting max borrow wei'
+      );
+
+      const setMarginPremiumData = await core.dolomiteMargin.populateTransaction.ownerSetMarginPremium(core.marketIds.usd1, { value: ONE_BI });
+      assertHardhatInvariant(
+        await core.ownerAdapterV2.isUserApprovedToSubmitTransaction(
+          adminSetRiskParams.address,
+          core.dolomiteMargin.address,
+          setMarginPremiumData.data!.slice(0,10)
+        ),
+        'Admin risk setter is not approved for setting market margin premium'
+      );
+
+      const setLiquidationPremiumData = await core.dolomiteMargin.populateTransaction.ownerSetLiquidationSpreadPremium(core.marketIds.usd1, { value: ONE_BI });
+      assertHardhatInvariant(
+        await core.ownerAdapterV2.isUserApprovedToSubmitTransaction(
+          adminSetRiskParams.address,
+          core.dolomiteMargin.address,
+          setLiquidationPremiumData.data!.slice(0,10)
+        ),
+        'Admin risk setter is not approved for setting market liquidation premium'
+      );
+
+      const setCategoriesByMarketIdsData = await core.dolomiteAccountRiskOverrideSetter.populateTransaction.ownerSetCategoriesByMarketIds(
+        [core.marketIds.usd1],
+        [0]
+      );
+      assertHardhatInvariant(
+        await core.ownerAdapterV2.isUserApprovedToSubmitTransaction(
+          adminSetRiskParams.address,
+          core.dolomiteAccountRiskOverrideSetter.address,
+          setCategoriesByMarketIdsData.data!.slice(0,10)
+        ),
+        'Admin risk setter is not approved for setting category by market ids'
+      );
+
+      const setCategoryByMarketId = await core.dolomiteAccountRiskOverrideSetter.populateTransaction.ownerSetCategoryByMarketId(
+        core.marketIds.usd1,
+        0
+      );
+      assertHardhatInvariant(
+        await core.ownerAdapterV2.isUserApprovedToSubmitTransaction(
+          adminSetRiskParams.address,
+          core.dolomiteAccountRiskOverrideSetter.address,
+          setCategoryByMarketId.data!.slice(0,10)
+        ),
+        'Admin risk setter is not approved for setting category by market id'
+      );
+
+      const setCategoryParamData = await core.dolomiteAccountRiskOverrideSetter.populateTransaction.ownerSetCategoryParam(
+        0,
+        { value: ONE_BI },
+        { value: ONE_BI },
+      );
+      assertHardhatInvariant(
+        await core.ownerAdapterV2.isUserApprovedToSubmitTransaction(
+          adminSetRiskParams.address,
+          core.dolomiteAccountRiskOverrideSetter.address,
+          setCategoryParamData.data!.slice(0,10)
+        ),
+        'Admin risk setter is not approved for setting category param'
+      );
+
+      const setRiskFeatureByMarketIdData = await core.dolomiteAccountRiskOverrideSetter.populateTransaction.ownerSetRiskFeatureByMarketId(
+        core.marketIds.usd1,
+        0,
+        BYTES_EMPTY
+      );
+      assertHardhatInvariant(
+        await core.ownerAdapterV2.isUserApprovedToSubmitTransaction(
+          adminSetRiskParams.address,
+          core.dolomiteAccountRiskOverrideSetter.address,
+          setRiskFeatureByMarketIdData.data!.slice(0,10)
+        ),
+        'Admin risk setter is not approved for setting risk feature'
+      );
+
+      await expect(adminSetRiskParams.connect(core.hhUser1).setMarketMaxBorrowWei(core.marketIds.usd1, 10)).to.be.reverted;
     },
   };
 }
